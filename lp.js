db0f7960ad020324056618a1a6a23d91d9ce88f6
Kartikishendurkar <116023614+Kartikishendurkar@users.noreply.github.com>
2022-10-18 13:04:06 +0530

    Create lp.js

---
+++ lp.js
@@ -0,0 +1,25 @@
+lpTag.sdes = lpTag.sdes || [];
+lpTag.identities = [];
+function identityFn(callback) {
+    callback({
+        iss: "Kartiki",
+        acr: "loa1",
+        sub: "1292157"
+    });
+}
+
+
+lpTag.sdes.push({
+    "type": "cart",
+    "total": 777,
+    "currency": "USD",
+    "numItems": "1",
+    "products": [{
+        "product": {
+            "name": "Title : Shop Online",
+            "category": " ",
+            "price": " "
+        }
+    }]
+})
+lpTag.identities.push(identityFn);
