3fc38ec149d0bc3d639be7a096d63ed0a4c2314b
Kartikishendurkar <116023614+Kartikishendurkar@users.noreply.github.com>
2022-10-18 13:03:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(async function () {
+
+  let pageTitle = 'Shop Online';
+  let cartTotal = 777;
+  appendData();
+
+
+  $('.home').click(function () {
+    localStorage.clear();
+    window.location.href = 'index.html';
+  })
+
+  $('#authenticated').click(function () {
+
+    window.location.href = 'authenticated.html';
+
+  })
+  $('#unauthenticated').click(function () {
+
+    localStorage.clear();
+    window.location.href = 'unauthenticated.html';
+
+  })
+
+
+  function appendData() {
+    $('#cartTotal').val(cartTotal);
+    $('#pageTitle').text(pageTitle)
+  }
+
+
+
+});
